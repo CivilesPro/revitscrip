6f849b1782604b9d13fd93c1599fd06244cf8b5e
CivilesPro <civilesprovalledupar@gmail.com>
2025-10-15 11:34:08 -0500

    actualizando al 2026

---
+++ Core/IsExternalInit.cs
@@ -0,0 +1,6 @@
+// Core/IsExternalInit.cs
+// Shim para permitir 'record' y 'init' en targets anteriores (net48).
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
